b38ce6c0aaa4d8e37cf8944d1253a4a158700f74
audreyBouet <audrey.bouet@cstb.fr>
2022-09-14 16:15:11 +0200

    path modification to data

--- harmonizer.py
+++ harmonizer.py
@@ -24,11 +24,10 @@ from rdflib import Graph
 #--start: Convert JSON to RDF---------------------------------------------------------------------------------------------------------
 #========================================================================================================================================= 
 # Convert a JSON file into a RDF or JSONLD file thanks to a mapping file (RML)
-def harmonizer(json_file, mapping_file, output_format):
+def harmonizer(json_file, mapping_file, output_format, output_file):
 
     #parsed_data = json.loads(pathlib.Path(json_file).read_text())
-    
-    output_file = json_file.split('.')[0] + '_output.' + output_format.lower()
+    #output_file = json_file.split('.')[0] + '_output.' + output_format.lower()
 
     # HARMONIZE DATA 
     if output_format.lower() == 'jsonld':
@@ -57,7 +56,9 @@ def harmonizer(json_file, mapping_file, output_format):
 if __name__ == "__main__":
 
     # Global Path
-    path_data = os.getcwd()
+    current_path = os.getcwd()
+    folder_data = 'Demo007'
+    path_data = os.path.join(current_path,'data', folder_data)
 
     # Arguments
     args = sys.argv
@@ -70,17 +71,21 @@ if __name__ == "__main__":
         JSON_data = args[1]
         RML_file = args[2]
         output_format = args[3] 
+
+        path_output = os.path.join(path_data,JSON_data.split('.')[0] + '_output.' + output_format.lower())
+        path_Json_data = os.path.join(path_data, JSON_data)
+        path_RML_file = os.path.join(path_data, RML_file)
         
         # Checks
-        if not os.path.exists(JSON_data):
+        if not os.path.exists(path_Json_data):
             print('Error, wrong JSON file')
             sys.exit()
-        elif not os.path.exists(RML_file):
+        elif not os.path.exists(path_Json_data):
             print('Error, wrong Mapping file')
             sys.exit()
         else:
             # Convert data to RDF
-            harmonizer(JSON_data,RML_file,output_format)
+            harmonizer(path_Json_data,path_RML_file,output_format,path_output)
 
 
 
