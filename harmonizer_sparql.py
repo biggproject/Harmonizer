ae47a382e3b5133eb1117955fb5920d62c77344b
audreyBouet <audrey.bouet@cstb.fr>
2022-10-10 13:38:02 +0200

    Addition of the SPARQL querry

--- harmonizer_sparql.py
+++ harmonizer_sparql.py
@@ -6,14 +6,15 @@
 #######################################
 
 
-import os 
+import os
+from random import gauss 
 import sys
 import pathlib
 import json
 import os
 from rdflib import Graph
 import pandas as pd
-from SPARQLWrapper import SPARQLWrapper, JSON
+from SPARQLWrapper import SPARQLWrapper, JSON, N3, TURTLE, JSONLD, RDF
 
 
 ######################################
@@ -25,13 +26,12 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 #--start: Convert JSON to RDF---------------------------------------------------------------------------------------------------------
 #========================================================================================================================================= 
 # Convert a JSON file into a RDF or JSONLD file thanks to a mapping file (RML)
-def harmonizer_sparql(filename):
+def harmonizer_sparql(filename, destination_file):
 
 
     # Specify the DBPedia endpoint
-    g = Graph()
-    result = g.parse(filename,format='n3')
-    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    g = Graph().parse(filename,format='n3')
+    #sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 
     # Default Query to add a source and the creation date
     query ="""
@@ -54,11 +54,12 @@ def harmonizer_sparql(filename):
             ?s ?p ?o .
         }
     """
-    results = g.query(query)
 
-    print('Results!')
-    df_result = pd.DataFrame(results, columns = results.vars)
-    print(df_result)
+    results = g.query(query)
+    g+= results
+
+    #print(g.serialize(format='ttl'))
+    g.serialize(destination=destination_file, format='ttl')
 
 #========================================================================================================================================= 
 #--end: Convert JSON to RDF---------------------------------------------------------------------------------------------------------
@@ -76,18 +77,25 @@ if __name__ == "__main__":
     path_data = os.getcwd()
 
     # Arguments
-    args = ['ll', "data_output.ttl"] #sys.argv
+    args = sys.argv #['Demo007', "data_output.ttl"] 
     
-    if len(args)!= 2:
+    if len(args)!= 4:
         print('Error: wrong number of arguments')
         sys.exit()
-    elif args[1].split(".")[1] != 'ttl':
+    elif args[2].split(".")[1] != 'ttl':
         print('Error: a ttl file is waiting')
         sys.exit()
-    elif os.path.exists(args[1]) == False : 
+    elif args[3].split(".")[1] != 'ttl':
+        print('Error: a ttl file is waiting')
+        sys.exit()
+    elif os.path.exists(os.path.join(path_data, 'data', args[1], args[2])) == False : 
         print("Error: the file does not exists")
         sys.exit()
     else :
-        filename = args[1]
-        harmonizer_sparql(filename)
+        demo_folder = args[1]
+        filename = args[2]
+        destination = args[3]
+        output_file = os.path.join(path_data, 'data', demo_folder, filename)
+        destination_filename = os.path.join(path_data, 'data', demo_folder, destination)
+        harmonizer_sparql(output_file, destination_filename)
 
